166f7c17d4ed0a08e5d76009f067bb1187cb29f8
MayBeCare <maybecare@yeah.net>
2018-04-23 11:05:09 +0800

    使用可视化工具SourceTree提交

--- src/test/java/com/ssm/cn/service/TaskServiceTest.java
+++ src/test/java/com/ssm/cn/service/TaskServiceTest.java
@@ -8,7 +8,8 @@ import static org.junit.Assert.*;
 public class TaskServiceTest {
 
     public static void main(String[] args) {
-         System.out.println("启动spring容器");
+        System.out.println("启动spring容器");
         ApplicationContext ac = new ClassPathXmlApplicationContext("classpath:spring/spring-*.xml");
+        System.out.println("spring容器启动完成");
     }
 }
